e59ed1819a399006456439ba1f5ca45871eef73c
Shahar Naveh <50263213+ShaharNaveh@users.noreply.github.com>
2024-12-01 14:59:43 +0200

    Update solve.py

--- 01/solve.py
+++ 01/solve.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python
+import pathlib
 
-DATA_FILE = "data.txt"
-with open(DATA_FILE) as fd:
+
+
+DATA_FILE = pathlib.Path(__file__).parent / "data.txt"
+
+with DATA_FILE.open() as fd:
   lines = fd.readlines()
 
 for line in lines:
