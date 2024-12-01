3a502e5c4acc9caac894b98ccd7c0ff40e3e9bfa
Shahar Naveh <50263213+ShaharNaveh@users.noreply.github.com>
2024-12-01 15:03:05 +0200

    Update solve.py

--- 01/solve.py
+++ 01/solve.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python
 import pathlib
 
-
-
 DATA_FILE = pathlib.Path(__file__).parent / "data.txt"
 
 with DATA_FILE.open() as fd:
   lines = fd.readlines()
 
+lst1 = []
+lst2 = []
+
 for line in lines:
+  line = line.strip()
+  
   print(line)
-  print(line.split(" "))
+  print(line.split(" " * 2))
   print(line.split("\t"))
   break
