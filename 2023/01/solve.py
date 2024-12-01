26b101a13d5be36e2b8077162d8522d2cad2c625
Shahar Naveh <50263213+ShaharNaveh@users.noreply.github.com>
2024-12-01 16:46:40 +0200

    Update solve.py

--- 2023/01/solve.py
+++ 2023/01/solve.py
@@ -15,6 +15,36 @@ def solve_p1(lst):
     res = found[0] + found[-1]
     yield int(res)
 
+def solve_p2(lst):
+  dct = {
+    "zero": "0",
+    "one": "1",
+    "two": "2",
+    "three": "3",
+    "four": "4",
+    "five": "5",
+    "six": "6",
+    "seven": "7",
+    "eight": "8",
+    "nine": "9"
+  }
+  joined="|".join(dct)
+  expr = fr"(\d|{joined})"
+  r = re.compile(expr)
+
+  for word in lst:
+    found = re.findall(r, word)
+    res = ""
+    for m in (found[0], found[-1]):
+      if m.isdigit():
+        res += m
+        continue
+      res += dct[m]
+    yield int(res)
+      
 p1_solution = sum(solve_p1(words))
 print(p1_solution)
+
+p2_solution = sum(solve_p2(words))
+print(p2_solution)
   
