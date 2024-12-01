8003d3d0e4cf0d76e9f5d0ef1e5f28dfe0df1b12
Shahar Naveh <50263213+ShaharNaveh@users.noreply.github.com>
2024-12-01 16:31:51 +0200

    Update solve.py

--- 2023/01/solve.py
+++ 2023/01/solve.py
@@ -1,8 +1,20 @@
 #!/usr/bin/env/python
 import pathlib
+import re
 
 DATA_FILE = pathlib.Path(__file__).parent / "data.txt"
 with DATA_FILE.open() as fd:
   lines = fd.readlines()
 
-lst = list(map(str.strip, lines))
+words = list(map(str.strip, lines))
+
+def solve_p1(lst):
+  r = re.compile(r"\d")
+  for word in lst:
+    found = re.findall(r, word)
+    res = found[0] + found[-1]
+    yield into(res)
+
+p1_solution = sum(solve_p1(words))
+print(p1_solution)
+  
