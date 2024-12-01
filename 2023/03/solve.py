c3c5c6bc4f4c7f8f56ebc90c1f609d754240c424
Shahar Naveh <50263213+ShaharNaveh@users.noreply.github.com>
2024-12-01 21:25:38 +0200

    Update solve.py

--- 2023/03/solve.py
+++ 2023/03/solve.py
@@ -5,3 +5,5 @@ INPUT_FILE = pathlib.Path(__file__).parent / "input.txt"
 def read():
   yield from input_file.read_text().splitlines()
 
+schematic = [cell for cell in cells.split('') for cells in read()]
+print(schematic)
