bd65d38ccb3888795c839c6b3969e326b9c3411e
Shahar Naveh <50263213+ShaharNaveh@users.noreply.github.com>
2024-12-01 17:46:41 +0200

    Update solve.py

--- 2023/02/solve.py
+++ 2023/02/solve.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python
 import pathlib
+import re
 
 DATA_FILE = pathlib.Path(__file__).parent / "data.txt"
 with DATA_FILE.open() as fd:
-  data = fd.readlines()
+  lines = fd.readlines()
   
+lines = map(str.strip, data)
+
+def parse_game_entry(line: str):
+  game_id = int(re.findall("Game (\d+)", line)[0])
+  game = {game_id: {}}
+  for color in ("red", "green", "blue"):
+    cubes = re.findall(fr"(\d+) {color}", line)
+    game[game_id][color] = cubes
+    
