b17cb1659efecf29aaae83205114774fba7548a1
Shahar Naveh <50263213+ShaharNaveh@users.noreply.github.com>
2024-12-01 17:50:48 +0200

    Update solve.py

--- 2023/02/solve.py
+++ 2023/02/solve.py
@@ -8,10 +8,17 @@ with DATA_FILE.open() as fd:
   
 lines = map(str.strip, data)
 
-def parse_game_entry(line: str):
+def parse_game_line(line: str):
   game_id = int(re.findall("Game (\d+)", line)[0])
   game = {game_id: {}}
+  
   for color in ("red", "green", "blue"):
     cubes = re.findall(fr"(\d+) {color}", line)
     game[game_id][color] = cubes
     
+  return game
+
+def is_game_possible(game) -> bool:
+  cubes_dict = game.values()
+  
+games = map(parse_game_line, lines)
