bb5a1b1bea1c316b5e763a20e7fdf1e969955aab
Shahar Naveh <50263213+ShaharNaveh@users.noreply.github.com>
2024-12-02 13:15:50 +0200

    Update solve.py

--- 2023/04/solve.py
+++ 2023/04/solve.py
@@ -2,16 +2,28 @@ import pathlib
 
 INPUT_FILE = pathlib.Path(__file__).parent / "input.txt"
 
-cards = INPUT_FILE.read_text().splitlines()
+data = INPUT_FILE.read_text().splitlines()
 
 def parse_line(line):
   title, numbers = line.split(":")
   card_id = int(title.split(" ")[1])
 
   winning, have = numbers.strip().split("|")
-  parse_numbers = lambda n: {int(x) for x in n.strip().split(" ") if x.strip() != ""}
+  parse_numbers = lambda n: [int(x) for x in n.strip().split(" ") if x.strip() != ""]
   winning = parse_numbers(winning)
   have = parse_numbers(have)
   entry = {card_id: {"winning": winning, "have": have}}
   yield entry
 
+def calc_won_points(card):
+  matches = 0
+  values = next(iter(card.values()))
+  winning, have = values["winning"], values["have"]
+  
+                
+  
+def solve_p1(cards):
+  for card in cards:
+    pass
+    
+cards = list(map(parse_line, data))
