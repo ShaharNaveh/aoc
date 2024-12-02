d9ff084fcd5568993d262e4fcf9f331d75e218e3
Shahar Naveh <50263213+ShaharNaveh@users.noreply.github.com>
2024-12-02 12:18:58 +0200

    Update solve.py

--- 2023/03/solve.py
+++ 2023/03/solve.py
@@ -8,4 +8,11 @@ for r_idx, row in enumerate(grid):
   for c_idx, char in enumerate(row):
     if char.isdigit() or char == ".":
       continue
+    # No we have a symbol
+    for cur_row in (r_idx - 1, r_idx, r_idx + 1):
+      for cur_col in (c_idx - 1, c_idx, c_idx + 1):
+        # Check if we OOB
+        if cur_row < 0 or cur_row >= len(grid):
+          continue
+        
     
