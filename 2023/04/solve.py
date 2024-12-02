44d79eaabd9687bc273ab2ec35151c14f9cd9c77
Shahar Naveh <50263213+ShaharNaveh@users.noreply.github.com>
2024-12-02 13:45:28 +0200

    Update solve.py

--- 2023/04/solve.py
+++ 2023/04/solve.py
@@ -18,13 +18,12 @@ def parse_line(line):
 def calc_won_points(card):
   values = next(iter(card.values()))
   winning, have = values["winning"], values["have"]
-  count = 0
-  for num in winning:
-    if num in have:
-      count += 1
-      
+  
+  overlap = winning & have
+  count = len(overlap)   
   mul = min(count, 2)
-  res = count * mul 
+  res = count * mul
+  
   return res
   
 def solve_p1(cards):
@@ -33,3 +32,16 @@ def solve_p1(cards):
 cards = list(map(parse_line, data))
 
 print(sum(solve_p1(cards)))
+
+inp = """
+Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
+""".strip().splitlines()
+
+tmp = list(map(parse_line, inp))
+
+print(sum(solve_p1(tmp)))
