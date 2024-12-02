b915b296a6020a349ca03b93cd06e178a3459684
Shahar Naveh <50263213+ShaharNaveh@users.noreply.github.com>
2024-12-02 11:34:41 +0200

    Update solve.py

--- 2023/03/solve.py
+++ 2023/03/solve.py
@@ -1,9 +1,9 @@
 import pathlib
 
 def read(path: pathlib.Path):
-  yield from path.read_text().splitlines()
+  yield from map(str.strip, path.read_text().splitlines())
   
 INPUT_FILE = pathlib.Path(__file__).parent / "input.txt"
 
-schematic = [cell for cells in read(INPUT_FILE) for cell in cells.split()]
+schematic = [list(line) for line in read(INPUT_FILE)]
 print(schematic)
