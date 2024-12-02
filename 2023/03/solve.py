0fbd6ebf9a2f6eb9703f07d21c987745da35d446
Shahar Naveh <50263213+ShaharNaveh@users.noreply.github.com>
2024-12-02 11:37:16 +0200

    Update solve.py

--- 2023/03/solve.py
+++ 2023/03/solve.py
@@ -6,4 +6,5 @@ def read(path: pathlib.Path):
 INPUT_FILE = pathlib.Path(__file__).parent / "input.txt"
 
 schematic = [list(line) for line in read(INPUT_FILE)]
-print(schematic)
+schematic_size = (sum(1 for _ in next(schematic)), len(schematic))
+print(schematic_size)
