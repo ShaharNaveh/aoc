a60277febb49466c890955313a98e68fc4d232f7
Shahar Naveh <50263213+ShaharNaveh@users.noreply.github.com>
2024-12-02 16:50:04 +0200

    Update solve.py

--- 2024/02/solve.py
+++ 2024/02/solve.py
@@ -7,10 +7,14 @@ data = inp_file.read_text().splitlines()
 levels = [list(map(int, line.split(" "))) for line in data]
 
 def is_safe(level):
-  if not (level == sorted(level)) or (level == sorted(level, reversed=True)):
+  slevel = sorted(level)
+  if not ((level == slevel) or (level == sorted(level, reversed=True))):
     return False
     
-  for n1, n2 in zip(level, level[1:]):
+  for n1, n2 in zip(slevel, slevel[1:]):
+    if n1 == n2:
+      return False
+      
     diff = abs(n1 - n2)
     if diff > 3:
       return False
