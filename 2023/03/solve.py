f753d6c57aa6482604170ca59184f34a708a2571
Shahar Naveh <50263213+ShaharNaveh@users.noreply.github.com>
2024-12-02 12:14:49 +0200

    Update solve.py

--- 2023/03/solve.py
+++ 2023/03/solve.py
@@ -1,37 +1,11 @@
-import itertools
 import pathlib
-
-def read(path: pathlib.Path):
-  yield from map(str.strip, path.read_text().splitlines())
-
-def neighbors(x, y, *, size):
-  x_bound, y_bound = size
-  for x2 in range(x-1, x+2):
-    for y2 in range(y-1, y+2):
-      if not (-1 < x <= x_bound):
-        continue
-        
-      if not (-1 < y <= y_bound):
-        continue
-
-      if not ((x != x2) or (y != y2)):
-        continue
-        
-      if not (0 <= x2 <= x_bound):
-        continue
-        
-      if not (0 <= y2 <= y_bound):
-        continue
-
-      yield (x2, y2)
-
-      
-    
 
 INPUT_FILE = pathlib.Path(__file__).parent / "input.txt"
 
-schematic = [list(line) for line in read(INPUT_FILE)]
-schematic_size = (sum(1 for _ in schematic[0]), len(schematic))
+grid = INOUT_FILE.read_text().splitlines()
 
-tmp = list(neighbors(5, 5, size=schematic_size))
-print(tmp)
+for r_idx, row in enumerate(grid):
+  for c_idx, char in enumerate(row):
+    if char.isdigit() or char == ".":
+      continue
+    
