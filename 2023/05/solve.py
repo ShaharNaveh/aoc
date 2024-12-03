5aeb704f2a2f353ce04b3b7b7cc5a8376b94ba27
Shahar Naveh <50263213+ShaharNaveh@users.noreply.github.com>
2024-12-03 18:32:08 +0200

    Update solve.py

--- 2023/05/solve.py
+++ 2023/05/solve.py
@@ -3,6 +3,33 @@ import pathlib
 from typing import TypedDict
 import re
 
+input_file = pathlib.Path(__file__).parent / "test_input.txt"
+
+def p1(path):
+  puzzle = path.read_text().strip()
+  seeds, *blocks = puzzle.split("\n" * 2)
+  seeds = list(map(int, seeds.split()))
+  for block in blocks:
+    ranges = []
+    for line in block.splitlines()[1:]:
+      info = list(map(int, line.split()))
+      ranges.append(info)
+      
+    buf = []
+    for seed in seeds:
+      for dest, src, range_len in ranges:
+        if src <= seed < src + range_len:
+          val = seed - src + dest
+          buf.append(val)
+          break
+      else:
+        buf.append(seed)
+    seeds = buf
+  print(min(seeds))
+
+p1(input_file)
+exit()
+
 class Record(TypedDict):
   src: str
   dest: str
