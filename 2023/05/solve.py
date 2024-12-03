7f691387aff13adde222d5b6bda15845bbdca475
Shahar Naveh <50263213+ShaharNaveh@users.noreply.github.com>
2024-12-03 11:15:08 +0200

    Update solve.py

--- 2023/05/solve.py
+++ 2023/05/solve.py
@@ -22,12 +22,37 @@ parse_raw(raw)
 
 
 test_inp = """
+seeds: 79 14 55 13
 
 seed-to-soil map:
-2988689842 4194451945 100515351
-2936009234 3353543976 52680608
+50 98 2
+52 50 48
 
 soil-to-fertilizer map:
-3961802244 3774724750 90737174
-3164426550 3931513861 70563571
-147221566 1279409424 704464""".strip()
+0 15 37
+37 52 2
+39 0 15
+
+fertilizer-to-water map:
+49 53 8
+0 11 42
+42 0 7
+57 7 4
+
+water-to-light map:
+88 18 7
+18 25 70
+
+light-to-temperature map:
+45 77 23
+81 45 19
+68 64 13
+
+temperature-to-humidity map:
+0 69 1
+1 0 69
+
+humidity-to-location map:
+60 56 37
+56 93 4
+"""
