f838175aeb6fe08f6dcc4033b878780afc83a28f
Shahar Naveh <50263213+ShaharNaveh@users.noreply.github.com>
2024-12-03 19:12:35 +0200

    Update solve.py

--- 2024/03/solve.py
+++ 2024/03/solve.py
@@ -6,8 +6,10 @@ input_file = pathlib.Path(__file__).parent / "input.txt"
 def p1(path):
   text = path.read_text()
   pattern = re.compile(r"mul\(\d{1,3},\d{1,3}\)", re.MULTILINE)
+  res = 0
   for m in re.findall(pattern, text):
-    print(m)
-  
+    n1, n2 = m.lstrip("mul(").rstrip(")").split(",")
+    res += n1 * n2
+  print(res)
 
 p1(input_file)
