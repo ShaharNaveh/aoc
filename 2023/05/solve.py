dbae185cfb0b4c4e0cc9b5fb260cfa8ccfe92720
Shahar Naveh <50263213+ShaharNaveh@users.noreply.github.com>
2024-12-03 11:54:19 +0200

    Update solve.py

--- 2023/05/solve.py
+++ 2023/05/solve.py
@@ -8,7 +8,8 @@ def parse_almanac_line(line: str):
 
 def parse_raw(raw: str):
   pattern = re.compile(r"(?P<section>[a-z-]+ map):\n(?P<data>(?:\d+ \d+ \d+\n?)+)", re.MULTILINE)
-  seed_line, _, raw_almanac = raw.partition("\n")
+  seed_line, _, raw_almanac = raw.strip().partition("\n")
+  print(seed_line)
   seeds_data = seed_line.split(":")[1].strip()
   seeds = set(map(int, seeds_data))
   result = {"seeds": seeds, "mappings": []}
