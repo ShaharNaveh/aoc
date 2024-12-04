4ee05e3053b5de718bdd3682e4a04908c1453ae6
Shahar Naveh <50263213+ShaharNaveh@users.noreply.github.com>
2024-12-04 15:05:18 +0200

    Update solve.py

--- 2024/04/solve.py
+++ 2024/04/solve.py
@@ -6,6 +6,7 @@ input_file = pathlib.Path(__file__).parent / "test_input.txt"
 def p1(path):
   inp = path.read_text().strip().lower()
   grid = [line.split() for line in inp.splitlines()]
+  print(grid)
   for col_idx, col in enumerate(grid):
     for row_idx, char in enumerate(col):
       if char == "x":
