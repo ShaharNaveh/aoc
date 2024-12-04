b68b2ed14f3bf00632e7a83b490093f1c351af4d
Shahar Naveh <50263213+ShaharNaveh@users.noreply.github.com>
2024-12-04 15:17:35 +0200

    Update solve.py

--- 2024/04/solve.py
+++ 2024/04/solve.py
@@ -1,4 +1,9 @@
 import pathlib
+from typing import NamedTuple
+
+class Pos(NamedTuple):
+  x: int
+  y: int
 
 input_file = pathlib.Path(__file__).parent / "input.txt"
 input_file = pathlib.Path(__file__).parent / "test_input.txt"
@@ -6,10 +11,17 @@ input_file = pathlib.Path(__file__).parent / "test_input.txt"
 def p1(path):
   inp = path.read_text().strip().lower()
   grid = [list(line) for line in inp.splitlines()]
+  bounds = Pos(x=len(grid[0]), y=len(grid))
+  start_poses = set()
+  
   for col_idx, col in enumerate(grid):
     for row_idx, char in enumerate(col):
-      if char == "x":
-        print((col_idx, row_idx))
+      if char != "x":
+        continue
+      pos = Pos(x=col_idx, y=row_idx)
+      start_poses.add(pos)
+
+  print(start_poses)
       
 
 p1(input_file)
