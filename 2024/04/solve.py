426b04db962b8ef32f100915eb746bd18e568e78
Shahar Naveh <50263213+ShaharNaveh@users.noreply.github.com>
2024-12-04 15:28:02 +0200

    Update solve.py

--- 2024/04/solve.py
+++ 2024/04/solve.py
@@ -1,7 +1,7 @@
 import pathlib
 from typing import NamedTuple
 
-class Pos(NamedTuple):
+class Cord(NamedTuple):
   x: int
   y: int
 
@@ -11,17 +11,24 @@ input_file = pathlib.Path(__file__).parent / "test_input.txt"
 def p1(path):
   inp = path.read_text().strip().lower()
   grid = [list(line) for line in inp.splitlines()]
-  bounds = Pos(x=len(grid[0]), y=len(grid))
-  start_poses = set()
   
+  start_cords = set()  
   for col_idx, col in enumerate(grid):
     for row_idx, char in enumerate(col):
       if char != "x":
         continue
-      pos = Pos(x=col_idx, y=row_idx)
-      start_poses.add(pos)
+      cord = Cord(x=col_idx, y=row_idx)
+      start_cords.add(cord)
 
-  print(start_poses)
+  bounds = Cord(x=len(grid[0]), y=len(grid))
+  directions = {
+    (-1, 0), # West
+    (1, 0), # East
+  }
+  res = 0
+  for start_cord in start_cords:
+    for direction in directions:
+      pass
       
 
 p1(input_file)
