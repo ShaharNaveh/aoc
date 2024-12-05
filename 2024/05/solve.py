310aae1ccc2fac3cf302a7068eea9a9f5ce67c8a
Shahar Naveh <50263213+ShaharNaveh@users.noreply.github.com>
2024-12-05 11:16:03 +0200

    Update solve.py

--- 2024/05/solve.py
+++ 2024/05/solve.py
@@ -1,9 +1,27 @@
 import pathlib
 
 
-def p1():
-  pass
+def p1(path):
+  inp = path.read_text().strip()
+  rule_block, update_block = inp.split("\n" * 2)
+  
+  rules = set()
+  for line in rule_block.splitlines():
+    rules |= set(map(int, line.split("|")))
+    
+  updates = set()
+  for line in update_block.splitlines():
+    updates |= {tuple(map(int, line.split(",")))}
+
+  for update in updates:
+    if not set(update).issubset(rules):
+      print(update)
+
+
+    
 
 
 
 input_file = pathlib.Path(__file__).parent / "input.txt"
+
+p1(input_file)
