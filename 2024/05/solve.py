a94349e598e3a2d0a9384c1d71bde537cc06e5e4
Shahar Naveh <50263213+ShaharNaveh@users.noreply.github.com>
2024-12-05 12:24:38 +0200

    Update solve.py

--- 2024/05/solve.py
+++ 2024/05/solve.py
@@ -1,5 +1,4 @@
 import pathlib
-import functools
 
 def is_in_order(update: list[int], rules: set) -> bool:
   for rule in rules:
@@ -11,6 +10,9 @@ def is_in_order(update: list[int], rules: set) -> bool:
       
   return True
 
+def fix_update(update: list[int], rules: set) -> list[int]:
+  pass
+  
 def middle_page(update: list[int]) -> int:
   idx = (len(update) - 1) // 2
   return update[idx]
@@ -21,11 +23,25 @@ def p1(path):
   
   rules = set(map(lambda line: complex(*map(int, line.split("|"))), rule_block.splitlines()))
   updates = [list(map(int, line.split(","))) for line in update_block.splitlines()]
-  func = functools.partial(is_in_order, rules=rules)
-  res = sum(map(middle_page, filter(func, updates)))
+  
+  good_updates = filter(lambda update: is_in_order(update, rules), updates)
+  res = sum(map(middle_page, good_updates))
+  print(res)
+
+def p2(path):
+  inp = path.read_text().strip()
+  rule_block, update_block = inp.split("\n" * 2)
+  
+  rules = set(map(lambda line: complex(*map(int, line.split("|"))), rule_block.splitlines()))
+  updates = [list(map(int, line.split(","))) for line in update_block.splitlines()]
+  
+  bad_updates = filter(lambda update: not is_in_order(update, rules), updates)
+  fixed_updates = map(lambda update: fix_update(update, rules), bad_updates)
+  res = sum(map(middle_page, fixed_updates))          
   print(res)
 
 input_file = pathlib.Path(__file__).parent / "input.txt"
-#input_file = pathlib.Path(__file__).parent / "test_input.txt"
+input_file = pathlib.Path(__file__).parent / "test_input.txt"
 
 p1(input_file)
+p2(input_file)
