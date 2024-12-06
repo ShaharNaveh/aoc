557de0425897a0eb9056cd224617353dd34e8ef8
Shahar Naveh <50263213+ShaharNaveh@users.noreply.github.com>
2024-12-06 21:18:46 +0200

    Update solve.py

--- 2023/07/solve.py
+++ 2023/07/solve.py
@@ -2,6 +2,11 @@ import pathlib
 
 def hand_strength(hand: str, card_strength: dict[str, int]) -> int:
   cards = list(hand)
+  order_strength = sum(
+    cards_strength[card] * (10 ** order)
+    for order, card in enumerate(reversed(cards), start=1)
+  )
+  
   
 def iter_puzzle(path):
   puzzle = path.read_text().strip()
