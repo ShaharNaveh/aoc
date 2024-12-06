1be259f13cc2d2b408186d218d322ba6c673177a
Shahar Naveh <50263213+ShaharNaveh@users.noreply.github.com>
2024-12-06 11:26:08 +0200

    Update solve.py

--- 2024/06/solve.py
+++ 2024/06/solve.py
@@ -1,4 +1,8 @@
 import pathlib
 
+
+def parse_puzzle(path):
+  inp = path.read_text().strip()
+  
 puzzle_file = pathlib.Path(__file__).parent / "puzzle.txt"
 puzzle_file = pathlib.Path(__file__).parent / "test_puzzle.txt"
