24fbe2e154a08e0d0ff110999d802c8739be0d6e
Shahar Naveh <50263213+ShaharNaveh@users.noreply.github.com>
2024-12-06 20:00:04 +0200

    Update solve.py

--- 2023/06/solve.py
+++ 2023/06/solve.py
@@ -1,4 +1,18 @@
 import pathlib
 
+
+def parse(path) -> set[complex]:
+  puzzle = path.read_text().strip()
+  #time_line, distance_line = puzzle.splitlines()
+  _, times, _ distances = puzzle.split(":")
+  times = map(int, times.split())
+  distances = map(int, distances.split())
+  races = {complex(time, distance) for time, distance in zip(times, distances)}
+  return races
+
+def p1(path):
+  races = parse(path)
+  print(races)
+
 puzzle_file = pathlib.Path(__file__).parent / "puzzle.txt"
 puzzle_file = pathlib.Path(__file__).parent / "test_puzzle.txt"
