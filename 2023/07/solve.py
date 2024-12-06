d444538ca3c2fb33cb03a43dd9efdc728529addd
Shahar Naveh <50263213+ShaharNaveh@users.noreply.github.com>
2024-12-06 21:09:41 +0200

    Update solve.py

--- 2023/07/solve.py
+++ 2023/07/solve.py
@@ -1,7 +1,13 @@
 import pathlib
 
-def parse(path):
+def hand_strength(hand: str, card_strength: dict[str, int]) -> int:
+  cards = list(hand)
   
+def iter_puzzle(path):
+  puzzle = path.read_text().strip()
+  for line in puzzle.splitlines():
+    hand, bid = line.split()
+    yield (hand, int(bid))
 
 
 puzzle_file = pathlib.Path(__file__).parent / "puzzle.txt"
