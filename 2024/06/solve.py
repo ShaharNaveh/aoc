c004b333aeb8191d4fa4064f5cfeedc6f4f3eb4a
Shahar Naveh <50263213+ShaharNaveh@users.noreply.github.com>
2024-12-06 17:45:08 +0200

    Update and rename grrrrr_solve.py to solve.py

--- 2024/06/solve.py
+++ 2024/06/solve.py
@@ -31,28 +31,21 @@ def parse_puzzle(path):
       cord = complex(row_idx, col_idx)
       cords[cord] = char
       if char not in {".", "#"}:
-        cords[cord] = "." # If there's a guard there, it's walkable
+        #cords[cord] = "." # If there's a guard there, it's walkable
         guard = cord
   return {"guard": guard, "cords": cords}
 
 def walked_cords(cord: complex, cords: dict[complex, str]) -> tuple[set[tuple[complex, Direction]], bool]:
   directions = itertools.cycle(Direction)
-  #direction = next(directions)
-  direction = -1
+  direction = next(directions)
   seen = set()
   while (cord in cords) and (cord, direction) not in seen:
     seen |= {(cord, direction)}
-    if cords.get(cord + direction) == "#":
-      direction *= -1j
-    else:
-      cord += direction
 
-    '''
     while cords.get(cord + direction.value) == "#":
-      direction = next(directions)
-      
+      direction = next(directions)    
     cord = cord + direction.value
-    '''
+  
   return set(map(operator.itemgetter(0), seen)), (cord, direction) in seen
   
 def p1(path):
@@ -67,22 +60,10 @@ def p2(path):
   seen, _ = walked_cords(cord, cords)
   print(
     sum(
-      walked_cords(cord, cords | {pos: "#"})[1]
+      walked_cords(cord, (cords | {pos: "#"}))[1]
       for pos in seen
     )
   )
-  '''
-  res = sum(
-    map(
-      operator.itemgetter(1),
-      (
-        walked_cords(cord, cords | {loc: "#"})
-        for loc in locs
-      )
-    )
-  )
-  print(res)
-  '''
   
 puzzle_file = pathlib.Path(__file__).parent / "puzzle.txt"
 #puzzle_file = pathlib.Path(__file__).parent / "test_puzzle.txt"
