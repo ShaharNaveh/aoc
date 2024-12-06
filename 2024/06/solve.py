7516475f11e00568f9d70da7d18f28123b5f322d
Shahar Naveh <50263213+ShaharNaveh@users.noreply.github.com>
2024-12-06 14:13:55 +0200

    Update solve.py

--- 2024/06/solve.py
+++ 2024/06/solve.py
@@ -1,2 +1,13 @@
 import functools
 import pathlib
+
+def parse_puzzle(path):
+  inp = path.read_text().strip()
+  return {
+    (row_idx, col_idx): char
+    for row_idx, row in enumerate(inp.splitlines())
+    for col_idx, char in enumerate(row)
+  }
+  
+puzzle_file = pathlib.Path(__file__).parent / "puzzle.txt"
+puzzle_file = pathlib.Path(__file__).parent / "test_puzzle.txt"
