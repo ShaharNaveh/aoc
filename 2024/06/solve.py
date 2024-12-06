07fbaa200a2652a5dbf0a834de78e48489072fcc
Shahar Naveh <50263213+ShaharNaveh@users.noreply.github.com>
2024-12-06 14:17:41 +0200

    Update solve.py

--- 2024/06/solve.py
+++ 2024/06/solve.py
@@ -8,6 +8,12 @@ def parse_puzzle(path):
     for row_idx, row in enumerate(inp.splitlines())
     for col_idx, char in enumerate(row)
   }
+
+def p1(path):
+  grid = parse_puzzle(path)
+  start_cord = next((cord for cord in grid if grid[cord] == "^"), None)
   
 puzzle_file = pathlib.Path(__file__).parent / "puzzle.txt"
 puzzle_file = pathlib.Path(__file__).parent / "test_puzzle.txt"
+
+p1(puzzle_file)
