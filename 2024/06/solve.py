b6cb29c2d2a72ee7a3166a5f85e3199a1a288950
Shahar Naveh <50263213+ShaharNaveh@users.noreply.github.com>
2024-12-06 11:32:44 +0200

    Update solve.py

--- 2024/06/solve.py
+++ 2024/06/solve.py
@@ -3,6 +3,14 @@ import pathlib
 
 def parse_puzzle(path):
   inp = path.read_text().strip()
+  grid = [list(line) for line in inp.splitlines()]
+  
+  for row_idx, row in enumerate(grid):
+    for col_idx, char in enumerate(row):
+      if char != "x":
+        continue
+      cord = Cord(x=row_idx, y=col_idx)
+      start_cords.add(cord)
   
 puzzle_file = pathlib.Path(__file__).parent / "puzzle.txt"
 puzzle_file = pathlib.Path(__file__).parent / "test_puzzle.txt"
