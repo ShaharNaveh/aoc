98b11547100b0655037d835e703ccfd2e7228246
Shahar Naveh <50263213+ShaharNaveh@users.noreply.github.com>
2024-12-06 14:24:32 +0200

    Update solve.py

--- 2024/06/solve.py
+++ 2024/06/solve.py
@@ -8,10 +8,25 @@ def parse_puzzle(path):
     for row_idx, row in enumerate(inp.splitlines())
     for col_idx, char in enumerate(row)
   }
-
+  
+def get_seen(cord, cords):
+    dir_iter = functools.cycle([(-1, 0), (0, 1), (1, 0), (0, -1)])
+    d = next(dir_iter)
+    p = cord
+    seen = set()
+    while p in cords and (p, d) not in seen:
+        seen.add((p, d))
+        while cords.get((p[0] + d[0], p[1] + d[1])) == '#':
+            d = next(dir_iter)
+        p = (p[0] + d[0], p[1] + d[1])
+    return seen, p in cords
+  
 def p1(path):
-  grid = parse_puzzle(path)
-  start_cord = next((cord for cord in grid if grid[cord] == "^"), None)
+  cords = parse_puzzle(path)
+  start_cord = next((cord for cord in cords if cords[cord] == "^"), None)
+  seen, _ = get_seen(cord=start_cord, cords=cords)
+  print(len(set(pos for pos, _ in seen)))
+
   
 puzzle_file = pathlib.Path(__file__).parent / "puzzle.txt"
 puzzle_file = pathlib.Path(__file__).parent / "test_puzzle.txt"
