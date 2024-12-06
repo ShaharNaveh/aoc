837bd85fd7555e8195a88c36bff60c7909f2610b
Shahar Naveh <50263213+ShaharNaveh@users.noreply.github.com>
2024-12-06 20:37:35 +0200

    Update solve.py

--- 2023/06/solve.py
+++ 2023/06/solve.py
@@ -29,6 +29,6 @@ def p1(path):
   print(res)
 
 puzzle_file = pathlib.Path(__file__).parent / "puzzle.txt"
-puzzle_file = pathlib.Path(__file__).parent / "test_puzzle.txt"
+#puzzle_file = pathlib.Path(__file__).parent / "test_puzzle.txt"
 
 p1(puzzle_file)
