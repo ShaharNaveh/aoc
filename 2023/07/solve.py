e8964fb15763e548e590c72847f57990fc80e09d
Shahar Naveh <50263213+ShaharNaveh@users.noreply.github.com>
2024-12-08 01:22:04 +0200

    Update solve.py

--- 2023/07/solve.py
+++ 2023/07/solve.py
@@ -21,26 +21,34 @@ class HandType(enum.IntEnum):
   FiveOfAKind = enum.auto()
 
   @classmethod
-  def detect(cls, hand: str):
+  def detect(cls, hand: str, *, is_p2: bool = False):
+    if is_p2:
+      jokers = hand.count("J")
+      hand = "".join(filter(lambda x: x != "J", hand))
+    else:
+      jokers = 0
     counts = sorted(collections.Counter(hand).values(), reverse=True)
-    
-    if counts[0] == 5:
+   
+    if not counts:
+     counts = counts[0]
+     
+    if counts[0] + jokers == 5:
       return cls.FiveOfAKind
 
-    if counts[0] == 4:
+    if counts[0] + jokers == 4:
       return cls.FourOfAKind
 
-    if (counts[0] == 3) and (counts[1] == 2):
+    if ((counts[0] + jokers == 3)) and (counts[1] == 2):
       return cls.FullHouse 
-    elif counts[0] == 3:
+    elif (counts[0] + jokers) == 3:
       return cls.ThreeOfAKind
 
     if all(count == 2 for count in counts[:2]):
       return cls.TwoPairs
-    elif counts[0] == 2:
+    elif (counts[0] + jokers) == 2:
       return cls.OnePair
 
-    if counts[0] == 1:
+    if (counts[0] + jokers) == 1:
       return cls.HighCard
       
     raise ValueError(f"Could not determine the hand type of: {repr(hand)}")     
@@ -79,9 +87,26 @@ def p1(path):
     
   print(res)
 
+
+def p2(path):
+  hands = iter_puzzle(path)
+  new_cards_strength = {**CARDS_STRENGTH, **{"J": -11}}
+  it = sorted(
+   hands, 
+   key=functools.cmp_to_key(
+    lambda a, b: cmp(a[0], b[0], cards_strength=new_cards_strength, is_p2=True)
+   )
+  )
+  res = 0
+  for rank, (_, bid) in enumerate(it, start=1):
+    bid = int(bid)
+    res += bid * rank
+    
+  print(res)
+
 puzzle_file = pathlib.Path(__file__).parent / "puzzle.txt"
 #puzzle_file = pathlib.Path(__file__).parent / "test_puzzle.txt"
 
 
 p1(puzzle_file)
-#p2(puzzle_file)
+p2(puzzle_file)
