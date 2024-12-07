337b958052a9132e83487b6411926c2c813381fe
Shahar Naveh <50263213+ShaharNaveh@users.noreply.github.com>
2024-12-07 12:00:50 +0200

    Update solve.py

--- 2023/07/solve.py
+++ 2023/07/solve.py
@@ -72,10 +72,11 @@ def iter_puzzle(path):
   puzzle = path.read_text().strip()
   for line in puzzle.splitlines():
     hand, bid = line.split()
+   # if hand not in {"KK677", 
     yield (hand, int(bid))
 
 def p1(path):
- it = sorted(iter_puzzle(path), key=lambda l: hand_strength(l[0], cards_strength=CARDS_STRENGTH))
+ it = sorted(iter_puzzle(path), key=lambda l: -hand_strength(l[0], cards_strength=CARDS_STRENGTH))
  res = 0
  for rank, (card, bid) in enumerate(it, start=1):
    print(f"{rank=}\t{card=}\t{bid=}")
