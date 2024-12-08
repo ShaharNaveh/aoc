5b5a7d890149f2e425b22f899f5b5199f0bf719d
Shahar Naveh <50263213+ShaharNaveh@users.noreply.github.com>
2024-12-08 16:28:30 +0200

    Update solve.py

--- 2023/07/solve.py
+++ 2023/07/solve.py
@@ -1,112 +1,67 @@
 import collections
-import enum
-import functools
 import pathlib
-import re
-#from typing import Sequence
 
-CARDS_STRENGTH = {
-  **{str(num): num for num in range(2, 10)},
-  **{symbol: idx for idx, symbol in enumerate(list("TJQKA"), start=10)},
-}
-
-@enum.unique
-class HandType(enum.IntEnum):
-  HighCard = enum.auto()
-  OnePair = enum.auto()
-  TwoPairs = enum.auto()
-  ThreeOfAKind = enum.auto()
-  FullHouse = enum.auto()
-  FourOfAKind = enum.auto()
-  FiveOfAKind = enum.auto()
-
-  @classmethod
-  def detect(cls, hand: str, *, is_p2: bool = False):
-    if is_p2:
-      jokers = hand.count("J")
-      hand = "".join(filter(lambda x: x != "J", hand))
-    else:
-      jokers = 0
-    counts = sorted(collections.Counter(hand).values(), reverse=True)
+def card_strength(card: str, is_p2: bool = False) -> int:
+ 	if card.isnumeric():   
+	   return int(card)
    
-    if not counts:
-     counts = counts[0]
+ 	dct = {c: idx for idx, c in enumerate("TJQKA", start=10)}
+  if is_p2:
+    dct["J"] = -1
+ 	return dct[card]
+
+class Hand:
+  __slots__ = ("_cards", "_bid", "_typ", "_is_p2")
+
+ 	def __init__(self, line: str, *, is_p2: bool = True):
+	  	raw, bid = line.split()
+	  	self._bid = int(bid)
+  		self._cards = tuple(card_strength(c, is_p2=is_p2) for c in raw)
+ 	 	self._typ = self.detect_type()
+    self._is_p2 = is_p2
+
+ 	def detect_type(self):
+	  	counter = collections.Counter(self._cards)
+  		highest = max(counter.values())
+  		if is_p2:
+	   		wilds = counter[1]
+		   	del counter[1]
+	   		highest = wilds
+		   	if counter:
+		    		highest += max(counter.values())
+
+	  	if highest == 5:
+		   	return 6 # Five of a kind
+  		elif highest == 4:
+		   	return 5 # Four of a kind
+  		elif len(counter) == 2:
+		   	return 4 # Full house
+  		elif highest == 3:
+		   	return 3 # Three of a kind
+  		elif len(counter) == 3:
+		   	return 2 # Two pair
+  		elif highest == 2:
+		   	return 1 # One pair
+	  	else:
+		   	return 0 # High card
      
-    if counts[0] + jokers == 5:
-      return cls.FiveOfAKind
-
-    if counts[0] + jokers == 4:
-      return cls.FourOfAKind
-
-    if ((counts[0] + jokers == 3)) and (counts[1] == 2):
-      return cls.FullHouse 
-    elif (counts[0] + jokers) == 3:
-      return cls.ThreeOfAKind
-
-    if all(count == 2 for count in counts[:2]):
-      return cls.TwoPairs
-    elif (counts[0] + jokers) == 2:
-      return cls.OnePair
-
-    if (counts[0] + jokers) == 1:
-      return cls.HighCard
-      
-    raise ValueError(f"Could not determine the hand type of: {repr(hand)}")     
-
-def cmp(a, b, *, cards_strength=None, is_p2: bool = False) -> int:
-  type_a = HandType.detect(a, is_p2=is_p2)
-  type_b = HandType.detect(b, is_p2=is_p2)
-
-  if type_a > type_b:
-    return 1
-  elif type_b > type_a:
-    return -1
-
-  cards_strength = cards_strength if cards_strength else CARDS_STRENGTH
-  for i, j in zip(a, b):
-    i_strength = cards_strength[i]
-    j_strength = cards_strength[j]
-    
-    if i_strength > j_strength:
-      return 1
-    elif j_strength > i_strength:
-      return -1
-  return 0
-
-def iter_puzzle(path):
-  puzzle = path.read_text().strip()
-  return re.findall(r"(\w{5}) (\d+)", puzzle)
+ 	def __lt__(self, other):
+	  	return self._typ < other._typ or (self._typ == other._typ and self._cards < other._cards)
 
 def p1(path):
-  hands = iter_puzzle(path)
-  it = sorted(hands, key=functools.cmp_to_key(lambda a, b: cmp(a[0], b[0])))
-  res = 0
-  for rank, (_, bid) in enumerate(it, start=1):
-    bid = int(bid)
-    res += bid * rank
-    
+  puzzle = path.read_text().strip()
+  hands = sorted(map(Hand, puzzle.splitlines()))
+  res = sum(rank * hand._bid for rank, hand in hands)
   print(res)
-
 
 def p2(path):
-  hands = iter_puzzle(path)
-  new_cards_strength = {**CARDS_STRENGTH, **{"J": -11}}
-  it = sorted(
-   hands, 
-   key=functools.cmp_to_key(
-    lambda a, b: cmp(a[0], b[0], cards_strength=new_cards_strength, is_p2=True)
-   )
-  )
-  res = 0
-  for rank, (_, bid) in enumerate(it, start=1):
-    bid = int(bid)
-    res += bid * rank
-    
+  puzzle = path.read_text().strip()
+  hands = sorted(map(lambda line: Hand(line, is_p2=True, puzzle.splitlines()))
+  res = sum(rank * hand._bid for rank, hand in hands)
   print(res)
-
+ 
 puzzle_file = pathlib.Path(__file__).parent / "puzzle.txt"
-#puzzle_file = pathlib.Path(__file__).parent / "test_puzzle.txt"
-
+puzzle_file = pathlib.Path(__file__).parent / "test_puzzle.txt"
 
 p1(puzzle_file)
 p2(puzzle_file)
