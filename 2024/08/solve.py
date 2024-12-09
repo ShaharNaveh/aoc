9c36b399beff7e0af1732eea334eb7ccfb8eaeb8
Shahar Naveh <50263213+ShaharNaveh@users.noreply.github.com>
2024-12-09 11:22:51 +0200

    Update solve.py

--- 2024/08/solve.py
+++ 2024/08/solve.py
@@ -2,44 +2,47 @@ import collections
 import itertools
 import pathlib
 
-def antinodes_pos(cords):
-    for pos1, pos2 in itertools.combinations(cords, 2):
-        yield pos1 + pos1 - pos2
-        yield pos2 + pos2 - pos1
-
 def parse_puzzle(path):
-    inp = path.read_text().strip()
-    grid = {}
+    inp = path.read_text().strip().replace("#", ".")
+    puzzle = collections.defaultdict(set)
     for row_idx, row in enumerate(inp.splitlines()):
         for col_idx, char in enumerate(row):
-            pos = complex(col_idx, row_idx)
-            grid[pos] = char
-    return grid
+            puzzle[char].add(row_idx + col_idx * 1j)
 
-def groupby_char(grid):
-    result = collections.defaultdict(set)
-    for pos, char in grid.items():
-        result[char] |= {pos}
-
-    return result
-
+    return dict(puzzle)
 
 def p1(path):
-    grid = parse_puzzle(path)
-    inverted_grid = groupby_char(grid)
+    puzzle = parse_puzzle(path)
+    all_pos = {pos for pos in itertools.chain.from_iterable(puzzle.values())}
+    del puzzle["."]
 
-    seen = set()
-    for char, cords in inverted_grid.items():
-        if char == ".":
-            continue
-        for antinode_pos in antinodes_pos(cords):
-            if not grid.get(antinode_pos):
-                continue
-            seen.add(antinode_pos)
+    anti = set()
+    for char, cords in puzzle.items():
+        for a, b in itertools.combinations(cords, 2):
+            diff = a - b
+            if (a := a + diff) in all_pos:
+                anti.add(a)
+            if (b := b - diff) in all_pos:
+                anti.add(b)
+    print(len(anti))
 
-    print(len(seen))
+def p2(path):
+    puzzle = parse_puzzle(path)
+    all_pos = {pos for pos in itertools.chain.from_iterable(puzzle.values())}
+    del puzzle["."]
+
+    anti = set()
+    for char, cords in puzzle.items():
+        for a, b in itertools.combinations(cords, 2):
+            diff = a - b
+            while (a := a - diff) in all_pos:
+                anti.add(a)
+            while (b := b + diff) in all_pos:
+                anti.add(b)
+    print(len(anti))
 
 puzzle_file = pathlib.Path(__file__).parent / "input.txt"
-#puzzle_file = pathlib.Path(__file__).parent / "test_input.txt"
+puzzle_file = pathlib.Path(__file__).parent / "test_input.txt"
 
 p1(puzzle_file)
+p2(puzzle_file)
