f055769415c0944194c94a2787d2dbdbe2e98d01
Shahar Naveh <50263213+ShaharNaveh@users.noreply.github.com>
2024-12-20 16:26:13 +0100

    Update solve.py

--- 2023/07/solve.py
+++ 2023/07/solve.py
@@ -1,18 +1,33 @@
 import collections
 import functools
-import pathlib                                                                                                                                                                                            BASE_REPLACE = {                                                                                         "A": "Z",                                                                                            "K": "Y",
+import pathlib
+
+BASE_REPLACE = {
+    "A": "Z",
+    "K": "Y",
     "T": "B",
 }
 
-                                                                                                     def cmp(v1, v2):                                                                                         if v1 > v2:                                                                                              return 1
+
+def cmp(v1, v2):
+    if v1 > v2:
+        return 1
     elif v1 < v2:
         return -1
     else:
-        return 0                                                                                                                                                                                          def cmp_hands(hand1, hand2, cmp_func):                                                                   h1, h2 = hand1[0], hand2[0]                                                                          cmp_h1, cmp_h2 = cmp_func(h1), cmp_func(h2)
+        return 0
+
+def cmp_hands(hand1, hand2, cmp_func):
+    h1, h2 = hand1[0], hand2[0]
+    cmp_h1, cmp_h2 = cmp_func(h1), cmp_func(h2)
     cmp_res = cmp(cmp_h1, cmp_h2)
     if cmp_res != 0:
         return cmp_res
-    return cmp(h1, h2)                                                                                                                                                                                    def p1_eval(hand: str):                                                                                  return tuple(sorted(collections.Counter(hand).values(), reverse=True))                           
+    return cmp(h1, h2)
+
+def p1_eval(hand: str):
+    return tuple(sorted(collections.Counter(hand).values(), reverse=True))
+
 def p2_eval(hand: str):
     if (jokers_count := hand.count("!")) == 5:
         return (5, )
@@ -45,12 +60,11 @@ def p2(puzzle_file):
     txt = inp
     for frm, to in replace.items():
         txt = txt.replace(frm, to)
-
+    
     return solve(txt, p2_eval)
-
 
 puzzle_file = pathlib.Path(__file__).parent / "input.txt"
 #puzzle_file = pathlib.Path(__file__).parent / "test_input.txt"
 
-print(p1(puzzle_file))               
+print(p1(puzzle_file))
 print(p2(puzzle_file))
