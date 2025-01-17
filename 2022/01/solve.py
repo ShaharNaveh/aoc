bb1b286f0ee56ed6d50e13513a9ecc346257fc0e
Shahar Naveh <50263213+ShaharNaveh@users.noreply.github.com>
2025-01-17 22:22:59 +0200

    Update solve.py

--- 2022/01/solve.py
+++ 2022/01/solve.py
@@ -1,1 +1,31 @@
+import pathlib
 
+def iter_puzzle(puzzle_file):
+    inp = puzzle_file.read_text().strip()
+    buf = []
+    for line in inp.splitlines():
+        if line == "":
+            yield buf
+            buf.clear()
+        else:
+            buf.append(int(line))
+    yield buf
+
+
+
+def p1(puzzle_file):
+    return max(map(sum, iter_puzzle(puzzle_file)))
+
+def p2(puzzle_file):
+    return sum(
+        sorted(
+            map(
+                sum, iter_puzzle(puzzle_file)
+            ),
+            reverse=True)[:3]
+    )
+
+puzzle_file = pathlib.Path(__file__).parent / "puzzle.txt"
+
+print(p1(puzzle_file))
+print(p2(puzzle_file))
