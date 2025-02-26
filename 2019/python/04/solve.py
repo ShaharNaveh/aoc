f3e14a451dccd4740b0f7f59252487f317387fa6
ShaharNaveh <50263213+ShaharNaveh@users.noreply.github.com>
2025-02-26 10:28:49 +0100

    Fmt

--- 2019/python/04/solve.py
+++ 2019/python/04/solve.py
@@ -1,7 +1,8 @@
 import itertools
 import pathlib
 
-def is_valid_password(num: int,*,is_p2: bool = False) -> bool:
+
+def is_valid_password(num: int, *, is_p2: bool = False) -> bool:
     s = str(num)
 
     if len(s) != 6:
@@ -9,7 +10,7 @@ def is_valid_password(num: int,*,is_p2: bool = False) -> bool:
 
     for _, group in itertools.groupby(s):
         size = len(list(group))
-        if (is_p2 and (size == 2)) or ((not is_p2) and (size>=2)):
+        if (is_p2 and (size == 2)) or ((not is_p2) and (size >= 2)):
             break
     else:
         return False
@@ -20,12 +21,10 @@ def is_valid_password(num: int,*,is_p2: bool = False) -> bool:
     return True
 
 
-
 def parse_puzzle(puzzle_file):
     inp = puzzle_file.read_text().strip()
-    start,stop = map(int, inp.split("-"))
-    return range(start, stop+1)
-
+    start, stop = map(int, inp.split("-"))
+    return range(start, stop + 1)
 
 
 def p1(puzzle_file):
@@ -33,7 +32,7 @@ def p1(puzzle_file):
 
 
 def p2(puzzle_file):
-    return sum(is_valid_password(num, is_p2=True) for num in  parse_puzzle(puzzle_file))
+    return sum(is_valid_password(num, is_p2=True) for num in parse_puzzle(puzzle_file))
 
 
 puzzle_file = pathlib.Path(__file__).parent / "puzzle.txt"
