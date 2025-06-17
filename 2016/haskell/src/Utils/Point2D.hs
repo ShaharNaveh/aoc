5cd243e553c3c9f3612ed5114f52007ad030cd6c
ShaharNaveh <50263213+ShaharNaveh@users.noreply.github.com>
2025-06-17 13:59:50 +0300

    Delete haskell

--- 2016/haskell/src/Utils/Point2D.hs
+++
@@ -1,9 +0,0 @@
-import Prelude hiding (sum)
-
-data Point2D = Point2D { x :: Int, y:: Int }
-    deriving (Eq, Read, Show)
-
-instance Num Point2D where
-    (Point2D x1 y1) + (Point2D x2 y2) = Point2D (x1 + x2) (y1 + y2)
-    -- (Point2D x1 y1) - (Point2D x2 y2) = Point2D (x1 - x2) (y1 - y2)
-    -- (Point2D x1 y1) * (Point2D x2 y2) = Point2D (x1 * x2) (y1 * y2)
